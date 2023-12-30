f16c33659eccc1a705c75cc27dc8c9484b00f949
Jordy <jordycorporan95@gmail.com>
2023-12-30 15:21:26 -0500

    add more directories to startprocess

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server Is Working")
+import express from "express";
+import helmet from 'helmet'
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
