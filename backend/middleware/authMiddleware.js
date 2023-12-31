1807afd0799f7b92a2a7a92c6cff239e9c5c2722
Jordy <jordycorporan95@gmail.com>
2023-12-30 21:54:41 -0500

    add generate session token, add functionality to controller functions

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
+
+// const protect = asyncHandler(async (req, res, next) => {
+//     let token
+
+//     if(req.headers.authorization && req.headers)
+// });
+
+export { protect };
