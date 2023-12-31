78f0beccbda255295d5a5b63d9b77ed72890f9eb
Jordy <jordycorporan95@gmail.com>
2023-12-31 12:37:42 -0500

    - Developed a simple logout mechanism by instructing the client to clear the token.
    - Enabled fetching and updating user profile details, including password updates with proper checks and validation.
    
    - Used asyncHandler for clean async error handling in all controller functions.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,27 +54,48 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Logout User",
-  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User Profile Fetched",
-  });
+  const user = {
+    _id: req.user_id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @desc Update user profile
 // @route PATCH /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User Profile Updated",
-  });
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not not found");
+  }
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 export {
   registerUser,
