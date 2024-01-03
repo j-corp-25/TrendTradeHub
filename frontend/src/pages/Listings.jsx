46d0230189c7d0c5af2d35d5584ee6b380f907a0
Manel-Oulds <oulds.manel@gmail.com>
2024-01-02 23:18:58 -0500

    use effect to fetch all the products

--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Product() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <div>Product</div>
   )
