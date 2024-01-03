2c4c52c73b673650ffdc77ee9df3b6cd95a63935
Manel-Oulds <oulds.manel@gmail.com>
2024-01-02 23:33:50 -0500

    Listing all the products working

--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../app/product';
 
 function Product() {
   const dispatch = useDispatch();
@@ -9,7 +10,14 @@ function Product() {
   }, [dispatch]);
 
   return (
-    <div>Product</div>
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
