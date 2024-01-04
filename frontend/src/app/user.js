dd18e6824b4492ff6d2a450dc2b529d4fb98aab7
Jordy <jordycorporan95@gmail.com>
2024-01-03 22:03:38 -0500

    create thunk actions and reducers

--- frontend/src/app/user.js
+++ frontend/src/app/user.js
@@ -6,6 +6,9 @@ export const LOGOUT = 'LOGOUT';
 export const SET_MESSAGE = 'SET_MESSAGE';
 export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
 
+const API_URL = "/api/users/register";
+const API_URL_LOGIN = "/api/users/login";
+
 export const registerSuccess = (user) => ({
     type: REGISTER_SUCCESS,
     payload: user,
@@ -36,3 +39,88 @@ export const registerSuccess = (user) => ({
   export const clearMessage = () => ({
     type: CLEAR_MESSAGE,
   });
+
+
+  export const register = (userData) => async (dispatch) => {
+    try {
+      const response = await axios.post(API_URL, userData);
+      if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        dispatch(registerSuccess(response.data));
+      }
+    } catch (error) {
+      dispatch(registerFail());
+      dispatch(setMessage(error.toString()));
+    }
+  };
+
+  export const login = (userData) => async (dispatch) => {
+    try {
+      const response = await axios.post(API_URL_LOGIN, userData);
+      if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        dispatch(loginSuccess(response.data));
+      }
+    } catch (error) {
+      dispatch(loginFail());
+      dispatch(setMessage(error.toString()));
+    }
+  };
+
+  export const performLogout = () => (dispatch) => {
+    localStorage.removeItem("user");
+    dispatch(logout());
+  };
+
+  const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    message: "",
+  };
+
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case REGISTER_SUCCESS:
+        return {
+          ...state,
+          isSuccess: true,
+          user: action.payload,
+        };
+      case REGISTER_FAIL:
+        return {
+          ...state,
+          isError: true,
+        };
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          isSuccess: true,
+          user: action.payload,
+        };
+      case LOGIN_FAIL:
+        return {
+          ...state,
+          isError: true,
+        };
+      case LOGOUT:
+        return {
+          ...state,
+          user: null,
+        };
+      case SET_MESSAGE:
+        return {
+          ...state,
+          message: action.payload,
+        };
+      case CLEAR_MESSAGE:
+        return {
+          ...state,
+          message: '',
+        };
+      default:
+        return state;
+    }
+  };
+
+  export default authReducer;
