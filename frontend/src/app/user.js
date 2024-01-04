d6da1bd9393de118f4cee27c431baccf6c1ad204
Jordy <jordycorporan95@gmail.com>
2024-01-03 22:01:27 -0500

    action creator and types

--- frontend/src/app/user.js
+++ frontend/src/app/user.js
@@ -5,3 +5,34 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const SET_MESSAGE = 'SET_MESSAGE';
 export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
+
+export const registerSuccess = (user) => ({
+    type: REGISTER_SUCCESS,
+    payload: user,
+  });
+
+  export const registerFail = () => ({
+    type: REGISTER_FAIL,
+  });
+
+  export const loginSuccess = (user) => ({
+    type: LOGIN_SUCCESS,
+    payload: user,
+  });
+
+  export const loginFail = () => ({
+    type: LOGIN_FAIL,
+  });
+
+  export const logout = () => ({
+    type: LOGOUT,
+  });
+
+  export const setMessage = (message) => ({
+    type: SET_MESSAGE,
+    payload: message,
+  });
+
+  export const clearMessage = () => ({
+    type: CLEAR_MESSAGE,
+  });
