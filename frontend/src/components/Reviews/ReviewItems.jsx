b87339faf8f2117d48c76ef596daf8beb115f08d
Jordy <jordycorporan95@gmail.com>
2024-01-05 15:03:57 -0500

    need to test frontend with product

--- frontend/src/components/Reviews/ReviewItems.jsx
+++ frontend/src/components/Reviews/ReviewItems.jsx
@@ -3,25 +3,48 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { BeatLoader } from "react-spinners";
-import { fetchReviews} from "../../app/reviews"
-import { useParams } from 'react-router-dom';
-import {
-    Container,
-  } from "react-bootstrap";
+import { fetchReviews } from "../../app/reviews";
+import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const ReviewItems = () => {
+  const dispatch = useDispatch();
+  const { productId } = useParams();
 
-    const dispatch = useDispatch();
-    const { productId } = useParams();
+  const { reviews, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.reviews
+  );
 
-    const {reviews,isLoading, isError, isSuccess, message } = useSelector (
-        (state) => state.reviews
-    )
+  useEffect(() => {
+    if (productId) {
+      dispatch(fetchReviews(productId));
+    }
+    if (isError) {
+      toast.error(message);
+    }
+  }, [dispatch, productId, isError, message]);
 
+  if (isLoading) {
+    return (
+      <Container className="my-6">
+        <div>
+          <BeatLoader />
+        </div>
+      </Container>
+    );
+  }
+
+  if (!reviews || reviews.length === 0) {
+    return <div>No reviews available for this product</div>;
+  }
   return (
+    <Container>
+      <h2>Reviews</h2>
+      {reviews.map((review) => (
+        <div key={review._id}>{review.comment}</div>
+      ))}
+    </Container>
+  );
+};
 
-    <div>Reviews</div>
-  )
-}
-
-export default ReviewItems
+export default ReviewItems;
