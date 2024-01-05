cbbdafed400c3479c6f58c931630509bf1117d68
Manel-Oulds <oulds.manel@gmail.com>
2024-01-05 11:42:30 -0500

    Product details fixed

--- frontend/src/components/ProductDetails.jsx
+++ frontend/src/components/ProductDetails.jsx
@@ -4,21 +4,42 @@ import "./ProductUnit.css";
 import { FaCartPlus, FaAngleRight } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchSingleProduct, fetchRelatedProducts } from "../app/product";
+import { fetchSingleProduct, fetchRelatedProducts, fetchProducts } from "../app/product";
 import ProductUnit from "./ProductUnit";
 
 function ProductDetails() {
-  const {productId} = useParams();
+  const { productId } = useParams();
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const dispatch = useDispatch();
   const product = useSelector((state) => state.products.selectedProduct) || {};
   const { title, price, images, _id } = product;
-  const relatedProducts = useSelector((state) => state.products.relatedProduct) || {};
+  const relatedProductIds = useSelector((state) => state.products.relatedProductIds);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchSingleProduct(productId));
-    dispatch(fetchRelatedProducts(productId));
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchSingleProduct(productId));
+        await dispatch(fetchRelatedProducts(productId));
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, [dispatch, productId]);
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      await dispatch(fetchProducts());
+      setLoading(false);
+    };
+
+    fetchAll();
+  }, [dispatch]);
 
   const handleNextImage = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -28,35 +49,37 @@ function ProductDetails() {
 
   return (
     <> 
-    <div className="product-details">
-      <div className="image-details">
-        {console.log(product)}
-        <img
-          src={
-            images?.length > 0 ? images[currentImageIndex] : "default-image-url"
-          }
-          alt={title}
-        />
-        {images?.length > 1 && (
-            <FaAngleRight className="right-arrow" onClick={handleNextImage} style={{opacity:"2", right:"5px", top:"50%"}} />
-          )}
-      </div>
-      <div className="prod-info">
-        <h1>{title}</h1>
-        <h2>${price}</h2>
-      </div>
-    </div>
+      {!loading && (
+        <div className="product-details">
+          <div className="image-details">
+            <img
+              src={
+                images[currentImageIndex] 
+              }
+              alt={title}
+            />
+            {images?.length > 1 && (
+              <FaAngleRight className="right-arrow" onClick={handleNextImage} style={{opacity:"2", right:"5px", top:"50%"}} />
+            )}
+          </div>
+          <div className="prod-info">
+            <h1>{title}</h1>
+            <h2>${price}</h2>
+          </div>
+        </div>
+      )}
 
-    <div className="sugg">
+      <div className="sugg">
         <h4>Related Products</h4>
-        {/* <div className="same-cat">
-        {relatedProducts && relatedProducts.map((relatedProduct) => (
-            <ProductUnit key={relatedProduct._id} product={relatedProduct} />
-          ))}
-        </div> */}
-    </div>
+        <div className="same-cat">
+        {console.log(relatedProductIds)}
+        {relatedProductIds.length > 0 &&
+            relatedProductIds?.map((relatedProductId) => (
+              <ProductUnit key={relatedProductId} productId={relatedProductId} />
+            ))}
+        </div>
+      </div>
     </>
-   
   );
 }
 
