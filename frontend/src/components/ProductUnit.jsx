b8d326db5b2b72c8e1fa8139d7773af5f64ca5d4
Manel-Oulds <oulds.manel@gmail.com>
2024-01-04 22:36:57 -0500

    listing working

--- frontend/src/components/ProductUnit.jsx
+++ frontend/src/components/ProductUnit.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './ProductUnit.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./ProductUnit.css";
+import { FaCartPlus, FaAngleRight } from "react-icons/fa";
 
+function ProductUnit({ product }) {
+  const { title, price, images } = product;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
-function ProductUnit({product}) {
-	return (
-
-                
-                <div class="product-item" >
-                    <div class="image">
-                        <a href="#/"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Product 4"/></a>
-                    </div>
-                    <div class="name">
-                        <a href="#/">Leather laptop bag</a>
-                    </div>
-                    <div className="price-like-cart">
-                        <div class="price">
-                            <span>$722.00</span>
-                        </div>
-                        <div className="cart">
-                            <button type="button" class="btn btn-primary">Add to Cart</button>
-                        </div>
-                        {/* <div className="like">
-                            <button type="button" class="btn btn-default wishlist" data-toggle="tooltip" data-placement="right" title="Wishlist"><i class="fa fa-heart"></i></button>
-
-                        </div> */}
-                        
-                    </div>
-                    
-                </div>
- 
-       	);
+  return (
+    <div className="product-item">
+      <div className="image">
+        <div className="content-image">
+          <a href="#/" onClick={handleNextImage}>
+            <img
+              src={images.length > 0 ? images[currentImageIndex] : "default-image-url"}
+              alt={title}
+            />
+          </a>
+          {images.length > 1 && (
+            <FaAngleRight className="right-arrow" onClick={handleNextImage} />
+          )}
+        </div>
+      </div>
+      <div className="name">
+        <a href="#/">{title}</a>
+      </div>
+      <div className="price-like-cart">
+        <div className="price">
+          <span>${price.toFixed(2)}</span>
+        </div>
+        <div className="cart">
+          <button
+            type="button"
+            className="btn btn-primary"
+            style={{ width: "70px", height: "25px" }}
+          >
+            <FaCartPlus />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default ProductUnit;
