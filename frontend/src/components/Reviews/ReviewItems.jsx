19639e535501766961d319dd657da5cf75a01cc8
Jordy <jordycorporan95@gmail.com>
2024-01-05 14:49:13 -0500

    working on review items

--- frontend/src/components/Reviews/ReviewItems.jsx
+++ frontend/src/components/Reviews/ReviewItems.jsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 import { BeatLoader } from "react-spinners";
+import { fetchReviews} from "../../app/reviews"
+import { useParams } from 'react-router-dom';
 import {
     Container,
   } from "react-bootstrap";
 
 const ReviewItems = () => {
+
+    const dispatch = useDispatch();
+    const { productId } = useParams();
+
+    const {reviews,isLoading, isError, isSuccess, message } = useSelector (
+        (state) => state.reviews
+    )
+
   return (
 
     <div>Reviews</div>
