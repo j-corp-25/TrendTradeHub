3f44ea090417bb6f523e234a77a98557b87c8458
Jordy <jordycorporan95@gmail.com>
2024-01-06 12:43:50 -0500

    working on styles for form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-// import "./reset.css";
-import "./index.css"
+// import "./index.css"
+import "./reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import configureStore from "./app/store";
 
