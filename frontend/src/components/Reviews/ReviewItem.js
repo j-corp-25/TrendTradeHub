81584868e8614ae2e9633bf3a7d36f40837c26d5
Manel-Oulds <oulds.manel@gmail.com>
2024-01-07 23:17:39 -0500

    Styling each review

---
+++ frontend/src/components/Reviews/ReviewItem.js
@@ -0,0 +1,52 @@
+// ReviewItem.js
+import React from "react";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+
+const ReviewItem = ({ review }) => {
+  const { author, rating, comment } = review;
+
+  const renderStars = (rating) => {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+
+    const starIcons = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      starIcons.push(<FaStar key={i} className="fa fa-star" />);
+    }
+
+    if (hasHalfStar) {
+      starIcons.push(
+        <FaStarHalfAlt key="half" className="fa fa-star-half-o" />
+      );
+    }
+
+    const remainingStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    for (let i = 0; i < remainingStars; i++) {
+      starIcons.push(<FaStar key={`empty-${i}`} className="fa fa-star-o" />);
+    }
+
+    return starIcons;
+  };
+
+  return (
+    <div className="review-item">
+      <div className="review-author">
+        {/* Display profile picture here */}
+        <img src={author.profilePicture} alt={`${author.name}'s profile`} />
+      </div>
+      <div className="review-content">
+        <div className="review-header">
+          <div className="user-info">
+            <div className="username">{author.name}</div>
+            <div className="rating">{renderStars(rating)}</div>
+          </div>
+        </div>
+        <div className="review-comment">{comment}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
