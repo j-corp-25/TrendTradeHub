c56fd6684fa465b52358fead99a4ee3c54936957
Manel-Oulds <oulds.manel@gmail.com>
2024-01-07 23:17:25 -0500

    Maping the reviews

--- frontend/src/components/Reviews/ReviewItems.jsx
+++ frontend/src/components/Reviews/ReviewItems.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// ReviewItems.js
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -6,6 +7,7 @@ import { BeatLoader } from "react-spinners";
 import { fetchReviews } from "../../app/reviewsReducer";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import ReviewItem from "./ReviewItem"; // Import the new component
 
 const ReviewItems = ({ productId }) => {
   const dispatch = useDispatch();
@@ -37,6 +39,7 @@ const ReviewItems = ({ productId }) => {
   if (!reviews || reviews.length === 0) {
     return <div>No reviews available for this product</div>;
   }
+
   return (
     <Container>
       <h2>Reviews</h2>
@@ -44,11 +47,7 @@ const ReviewItems = ({ productId }) => {
 
       <h2>Average Review: {averageRating}</h2>
       {reviews.map((review) => (
-        <div key={review._id}>
-          <p>Comment: {review.comment}</p>
-          <p>Rating: {review.rating}</p>
-          <p>User: {review.author.name}</p>
-        </div>
+        <ReviewItem key={review._id} review={review} />
       ))}
     </Container>
   );
