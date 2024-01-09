d385ea92e2d475eff17f8d29edd9981d754256e5
Jordy <jordycorporan95@gmail.com>
2024-01-09 10:24:25 -0500

    removed reset since its not needed in componenet

--- frontend/src/components/Products/ProductItems.jsx
+++ frontend/src/components/Products/ProductItems.jsx
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 // import { BeatLoader } from "react-spinners";
 import { Container } from "react-bootstrap";
 import fetchProducts from "../../app/productReducer";
-import { reset } from "../../app/userReducer";
+import { useNavigate } from "react-router-dom";
 
 function ProductItems() {
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
   useEffect(() => {
-    // Reset the state when the component mounts
-    dispatch(reset());
-
-    // Fetch products
     dispatch(fetchProducts());
-
   }, [dispatch]);
   return (
     <Container>
@@ -25,7 +21,7 @@ function ProductItems() {
           <div key={product._id}>
             <h3>{product.title}</h3>
             <p>Price: ${product.price}</p>
-            {/* <button onClick={() => navigate(`/product/${product._id}`)}>View Details</button> */}
+            <button onClick={() => navigate(`/product/${product._id}`)}>View Details</button>
           </div>
         ))}
       </div>
