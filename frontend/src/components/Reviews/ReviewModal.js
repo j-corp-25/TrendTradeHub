05607969753bb5fa2f74d6562a98438a331f4807
Manel-Oulds <oulds.manel@gmail.com>
2024-01-08 23:47:41 -0500

    Error handler

--- frontend/src/components/Reviews/ReviewModal.js
+++ frontend/src/components/Reviews/ReviewModal.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, Alert } from "react-bootstrap";
 import Rating from "react-rating-stars-component";
 import ReactStars from "react-rating-stars-component";
+import { createReview, fetchReviews } from "../../app/reviewsReducer";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../../app/productReducer";
+import { useEffect } from "react";
 
 const ReviewModal = ({ showModal, handleClose }) => {
+  const dispatch = useDispatch();
+  const userId = useSelector(state=> state.auth.user._id);
+  const product = useSelector(state=> state.products.selectedProduct?._id);
   const [comment, setComment] = useState("");
   const [rating, setRating] = useState(0); 
+  const [error, setError] = useState("");
 
 
   const handleSubmit = () => {
-    console.log("Comment:", comment);
-    handleClose();
+    if (rating > 0 && comment.trim() !== "") {
+    dispatch(createReview({ author: userId ,comment: comment, rating: rating, product: product}));
+    handleClose();}
+    else {
+      setError("Please Prodive both rating and comment");
+    }
   };
   const ratingChanged = (newRating) => {
-    console.log(newRating);
+    setRating(newRating);
   };
+
 
   return (
     <Modal show={showModal} onHide={handleClose}>
@@ -47,6 +60,10 @@ const ReviewModal = ({ showModal, handleClose }) => {
               value={comment}
               onChange={(e) => setComment(e.target.value)}
             />
+            {error && (
+              <Alert variant="danger" className="mt-2">
+                {error}
+              </Alert>)}
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -57,6 +74,7 @@ const ReviewModal = ({ showModal, handleClose }) => {
         <Button variant="primary" onClick={handleSubmit}>
           Submit Review
         </Button>
+        
       </Modal.Footer>
     </Modal>
   );
