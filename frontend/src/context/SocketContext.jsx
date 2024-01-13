8f84a58b5f6168d7789fbb0d76d40365acea5407
Jordy <jordycorporan95@gmail.com>
2024-01-13 16:18:57 -0500

    add connection to check if user connected

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,22 +1,29 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import io from "socket.io-client"
+import io from "socket.io-client";
 const SocketContext = createContext();
 
+
+
+export const useSocket = () => {
+    return useContext(SocketContext)
+}
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const user = useSelector((state) => state.auth.user);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:4000");
-    setSocket(newSocket);
+    const socket = io("http://localhost:4000", {
+      query: {
+        userId: user?._id,
+      },
+    });
+    setSocket(socket);
 
-    return () => newSocket.close();
-  }, []);
+    return () => socket && socket.close();
+  }, [user?._id]);
 
-  return;
-
-  <SocketContext.Provider value={"hi"}>{children}</SocketContext.Provider>;
+  return <SocketContext.Provider value={{socket}}>{children}</SocketContext.Provider>;
 };
 
 export default SocketContext;
