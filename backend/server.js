b938368b0b8affe6c9fa318c9807c180713a0a5a
Jordy <jordycorporan95@gmail.com>
2024-01-13 17:18:21 -0500

    online users showing

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
-dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js";
 import cors from "cors";
-import {upload} from "./middleware/multer.js"
-const port = process.env.PORT || 4000;
+import { upload } from "./middleware/multer.js";
+import {app,server} from "./sockets/socket.js"
+dotenv.config();
 connectDB();
+const port = process.env.PORT || 4000;
 
-const app = express();
+
 app.use(cors());
 
 app.use(express.json());
@@ -29,4 +30,4 @@ app.use("/api/messages", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.listen(port, () => console.log(`Server started on port ${port}`));
--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -11,8 +11,15 @@ const io = new Server(server, {
   },
 });
 
+const userSocketMap = {};
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+  io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 export { io, server, app };
