01c47ad31f35ebee6415f25282dd222a6ef4b46e
Manel-Oulds <oulds.manel@gmail.com>
2024-01-29 13:22:20 -0500

    Styling header listing page

--- frontend/src/pages/ListingsPage.jsx
+++ frontend/src/pages/ListingsPage.jsx
@@ -15,19 +15,25 @@ function Product() {
   useEffect(() => dispatch(fetchUsers()), []);
 
   return (
-    <>
-      {" "}
-      <div>
-        <div className="cloud">
-          {/* <img src={image} alt="trade-hub" /> */}
-        </div>
+    <div className="product-container">
+      <div className="product-header">
+        <div className="product-text">
+        <h2 style={{fontFamily:'font-family: Phantomsans, sans-serif;', color:'white'}}>Trade Smart, Trade Simple</h2>
+<p style={{fontFamily:'font-family: Phantomsans, sans-serif;', color:'gray', marginLeft:"20px"}}>Buy what you need, sell what you don't. <br />Your one-stop hub for seamless transactions.<br />
+   Explore, trade, and discover new possibilities today!</p>
+   <button class="button-63" role="button">Sell Product</button>
       </div>
+      <div className="cloud">
+        <img src={image} alt="trade-hub" />
+      </div>
+      </div>
+      
       <div className="product-list">
         {products.map((product) => (
           <ProductUnit key={product._id} productId={product._id} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
