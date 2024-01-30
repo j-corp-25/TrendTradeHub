ddc783c45445098e6cc083c45c3cd56361c527ad
Manel-Oulds <oulds.manel@gmail.com>
2024-01-29 22:37:55 -0500

    Add product working

--- frontend/src/components/Products/NewProduct.jsx
+++ frontend/src/components/Products/NewProduct.jsx
@@ -46,74 +46,74 @@ const NewProduct = () => {
   };
 
   return (
-    // <div>
-    //   <h1>Add Product</h1>
-    //   <div>
-    //     <label>Author:</label>
-    //     <input type="text" name="author" onChange={handleInputChange} />
-    //   </div>
-    //   <div>
-    //     <label>Title:</label>
-    //     <input type="text" name="title" onChange={handleInputChange} />
-    //   </div>
-    //   <div>
-    //     <label>Condition:</label>
-    //     <input type="text" name="condition" onChange={handleInputChange} />
-    //   </div>
-    //   <div>
-    //     <label>Price:</label>
-    //     <input type="text" name="price" onChange={handleInputChange} />
-    //   </div>
-    //   <div>
-    //     <label>Category:</label>
-    //     <input type="text" name="category" onChange={handleInputChange} />
-    //   </div>
-    //   <div>
-    //     <label>Images:</label>
-    //     <input type="file" name="myPic" onChange={handleImageChange} multiple />
-    //   </div>
-    //   <button onClick={handleAddProduct}>Add Product</button>
-    // </div>
-     <form className="form-container">
-     <div className="input-container">
-       <label htmlFor="base-input" className="label">
-         Title
-       </label>
-       <input type="text" id="base-input" className="input" name="title" onChange={handleInputChange} />
-     </div>
-     <div className="input-container">
-       <label htmlFor="large-input" className="label">
-         Description
-       </label>
-       <input type="text" id="large-input" className="input" />
-     </div>
-     <div className="input-container">
-       <label htmlFor="small-input" className="label">
-         Condition
-       </label>
-       <input type="text" id="small-input" className="input" />
-     </div>
-     <div className="input-container">
-       <label htmlFor="small-input" className="label">
-         Price
-       </label>
-       <input type="text" id="small-input" className="input" />
-     </div>
-     <div className="input-container">
-       <label htmlFor="small-input" className="label">
-         Cathegory
-       </label>
-       <input type="text" id="small-input" className="input" />
-     </div>
-     <div className="input-container">
-       <label htmlFor="small-input" className="label">
-       </label>
-       <input type="file" name="myPic" onChange={handleImageChange} multiple id="small-input" className="input" style={{background:"Gray"}} />
+    <div>
+      
+      <div>
+        <label className="label">Author:</label>
+        <input className="input" type="text" name="author" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label className="label">Title:</label>
+        <input className="input" type="text" name="title" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label className="label">Condition:</label>
+        <input className="input" type="text" name="condition" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label className="label">Price:</label>
+        <input className="input" type="text" name="price" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label className="label">Category:</label>
+        <input  className="input" type="text" name="category" onChange={handleInputChange} />
+      </div>
+      <div>
+        {/* <label className="label">Images:</label> */}
+        <input className="input" type="file" name="myPic" onChange={handleImageChange} multiple />
+      </div>
+      <button onClick={handleAddProduct} className="button-72">Add Product</button>
+    </div>
+  //    <form className="form-container">
+  //    <div className="input-container">
+  //      <label htmlFor="base-input" className="label">
+  //        Title
+  //      </label>
+  //      <input type="text" id="base-input" className="input" name="title" onChange={handleInputChange} />
+  //    </div>
+  //    <div className="input-container">
+  //      <label htmlFor="large-input" className="label">
+  //        Description
+  //      </label>
+  //      <input type="text" id="large-input" className="input" />
+  //    </div>
+  //    <div className="input-container">
+  //      <label htmlFor="small-input" className="label">
+  //        Condition
+  //      </label>
+  //      <input type="text" id="small-input" className="input" />
+  //    </div>
+  //    <div className="input-container">
+  //      <label htmlFor="small-input" className="label">
+  //        Price
+  //      </label>
+  //      <input type="text" id="small-input" className="input" />
+  //    </div>
+  //    <div className="input-container">
+  //      <label htmlFor="small-input" className="label">
+  //        Cathegory
+  //      </label>
+  //      <input type="text" id="small-input" className="input" />
+  //    </div>
+  //    <div className="input-container">
+  //      <label htmlFor="small-input" className="label">
+  //      </label>
+  //      <input type="file" name="myPic" onChange={handleImageChange} multiple id="small-input" className="input" style={{background:"Gray"}} />
 
-     </div>
-       <button class="button-72" role="button" onClick={handleAddProduct}>Add Product</button>
+  //    </div>
+  //      <button class="button-72" role="button" onClick={handleAddProduct}>Add Product</button>
 
-   </form>
+  //  </form>
   );
 };
 
