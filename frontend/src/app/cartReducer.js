024d50b15418fe75fc40ece8abee81778a0ad47c
Manel-Oulds <oulds.manel@gmail.com>
2024-02-05 11:59:18 -0500

    Cart reducer created

---
+++ frontend/src/app/cartReducer.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/api/users";
+
+// Action Types
+export const FETCH_CART_SUCCESS = "FETCH_CART_SUCCESS";
+export const ADD_CART_SUCCESS = "ADD_CART_SUCCESS";
+export const REMOVE_CART_SUCCESS = "REMOVE_CART_SUCCESS";
+
+// Action Creators
+export const fetchCartSuccess = (products) => ({
+  type: FETCH_CART_SUCCESS,
+  payload: products,
+});
+
+export const addCartSuccess = (product) => ({
+  type: ADD_CART_SUCCESS,
+  payload: product,
+});
+
+export const removeCartSuccess = (product) => ({
+  type: REMOVE_CART_SUCCESS,
+  payload: product,
+});
+
+// Thunk Action Creator
+export const fetchCart = (userId) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${API_URL}/getCartContent`, { userId });
+    dispatch(fetchCartSuccess(response.data));
+  } catch (error) {
+    console.error("Error fetching cart content:", error);
+  }
+};
+
+export const addToCart = (productId, userId) => async (dispatch) => {
+  try {
+    await axios.post(`${API_URL}/addToCart`, { productId, userId });
+    dispatch(addCartSuccess(productId));
+  } catch (error) {
+    console.error("Error adding product to cart:", error);
+  }
+};
+
+export const removeFromCart = (productId, userId) => async (dispatch) => {
+  try {
+    await axios.post(`${API_URL}/removeFromCart`, { productId, userId });
+    dispatch(removeCartSuccess(productId));
+  } catch (error) {
+    console.error("Error removing product from cart:", error);
+  }
+};
+
+// Reducer
+const initialState = {
+  cart: [],
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CART_SUCCESS:
+      return {
+        ...state,
+        cart: action.payload,
+      };
+
+    case ADD_CART_SUCCESS:
+      return {
+        ...state,
+        cart: [...state.products, action.payload],
+      };
+
+    case REMOVE_CART_SUCCESS:
+      return {
+        ...state,
+        cart: state.products.filter((productId) => productId !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
