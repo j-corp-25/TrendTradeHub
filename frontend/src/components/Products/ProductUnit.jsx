b3bab622578082b0a73ade71bf3cd440b4e610b8
Manel-Oulds <oulds.manel@gmail.com>
2024-02-17 20:41:53 -0500

    Description added

--- frontend/src/components/Products/ProductUnit.jsx
+++ frontend/src/components/Products/ProductUnit.jsx
@@ -13,7 +13,7 @@ function ProductUnit({ productId }) {
   const products = useSelector((state) => state.products.products) || [];
   const selectedProduct =
     products.find((product) => product._id === productId) || {};
-  const { title, price, images } = selectedProduct;
+  const { title, price, images, description } = selectedProduct;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const reviewsState = useSelector((state) => state.reviews);
   const sessionUser = useSelector((state) => state.auth.user);
@@ -89,6 +89,11 @@ function ProductUnit({ productId }) {
             ${price.toFixed(2)}
           </span>
         </div>
+      </div>
+
+      {/* Description with animation */}
+      <div className="description" style={{ color: "red", animation: "slideRight 0.5s forwards" }}>
+        {description}
       </div>
     </div>
   );
